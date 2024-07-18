97f7c97cd50316f24a18bf011fd6c9b4014f732b
Noah Xiren <voldigoadanos521@gmail.com>
2024-07-18 11:04:29 +0545

    basic constants realted to pproject

---
+++ scripts/Constants.py
@@ -0,0 +1,26 @@
+SIZE = {
+    "width": 800,
+    "height": 800,
+}
+
+COLOR = {
+    "BLACK": "#000000",
+    "white": "#ffffff",
+    "red": "#ff0000",
+    "green": "#00ff00",
+    "blue": "#0000ff",
+    "yellow": "#ffff00",
+    "cyan": "#00ffff",
+    "bg_color": "#000000",
+}
+
+PADDING = {
+    "top": 100,
+    "side": 100,
+}
+
+GRADIENTS = [
+                ("#383838"),
+                 ("#484848"),
+                 ("#606060")
+]
